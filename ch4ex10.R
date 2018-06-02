e0e46c612f91728fac8e701b5acc6d44220c8441
Pedro Varangot <peter@satellogic.com>
2018-06-02 18:59:32 -0300

    Chapter 4 exercise 10

---
+++ ch4ex10.R
@@ -0,0 +1,56 @@
+library(ISLR)
+attach(Weekly)
+?Weekly
+pairs(Weekly)
+summary(Weekly)
+plot(Today)
+
+# b, logistic regression
+glm.fits = glm(Direction~Lag1+Lag2+Lag3+Lag4+Lag5+Volume,
+               family = binomial)
+summary(glm.fits)
+glm.fits2 = glm(Direction~Lag2,
+               family = binomial)
+summary(glm.fits2)
+
+# c
+glm.probs = predict(glm.fits, Weekly[2:7], type = "response")
+length(glm.probs)
+glm.pred = rep("Down", length(glm.probs))
+glm.pred[glm.probs > .5] = "Up"
+table(glm.pred, Direction)
+(557+54)/length(glm.pred)
+mean(glm.pred == Direction)
+
+train = Year < 2009
+glm.fits3 = glm(Direction~Lag2, data=Weekly, 
+                subset = train, family = binomial)
+glm.probs3 = predict(glm.fits3, data.frame(Lag2=Lag2[!train]),
+                    type = "response")
+glm.pred3 = rep("Down", length(glm.probs3))
+glm.pred3[glm.probs3 > .5] = "Up"
+table(glm.pred3, Direction[!train])
+mean(glm.pred3 == Direction[!train])
+
+library(MASS)
+lda.fit = lda(Direction~Lag2, data=Weekly, subset=train)
+lda.pred = predict(lda.fit, data.frame(Lag2=Lag2[!train]), type="response")$class
+table(lda.pred, Direction[!train])
+mean(lda.pred == Direction[!train])
+
+qda.fit = qda(Direction~Lag2, data=Weekly, subset = train)
+qda.pred = predict(qda.fit, data.frame(Lag2=Lag2[!train]), type="response")
+table(qda.pred$class, Direction[!train])
+mean(qda.pred$class == Direction[!train])
+
+train.X = as.matrix(Lag2[train])
+test.X = as.matrix(Lag2[!train])
+train.Y = Direction[train]
+test.Y = Direction[!train]
+
+library(class)
+?knn
+set.seed(1)
+knn.pred = knn(train.X, test.X, train.Y, k=1)
+table(knn.pred, test.Y)
+mean(knn.pred == test.Y)
